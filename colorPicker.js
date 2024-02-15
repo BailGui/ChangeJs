3b7b1dea47090b06767b465c4e6f6644b380ff1b
Guillaume <guillaumebaillieux88@gmail.com>
2024-02-15 14:45:21 +0100

    maj150224

---
+++ colorPicker.js
@@ -0,0 +1,28 @@
+let body = document.querySelector("body");
+let backgroundColorPicker = document.querySelector("#backgroundColorPicker");
+let textColorPicker = document.querySelector("#textColorPicker");
+
+let originalBackgroundColor = localStorage.getItem("colorBackground");
+body.style.backgroundColor = originalBackgroundColor;
+backgroundColorPicker.value = originalBackgroundColor;
+
+let originalTextColor = localStorage.getItem("colorText");
+body.style.color = originalTextColor;
+textColorPicker.value = originalTextColor;
+
+function changeColorBackground() {
+    let newColor = backgroundColorPicker.value;
+    body.style.backgroundColor = newColor;
+
+    localStorage.setItem("colorBackground", newColor);
+};
+
+function changeColorText() {
+    let newColor = textColorPicker.value;
+    body.style.color = newColor;
+
+    localStorage.setItem("colorText", newColor);
+};
+
+backgroundColorPicker.addEventListener('change', changeColorBackground);
+textColorPicker.addEventListener('change', changeColorText);
